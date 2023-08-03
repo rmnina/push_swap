34625e0c1dcac27bbe6d0bc49ce32fb94898710e
jovica pc fixe <jovicadufour@gmail.com>
2023-08-03 18:53:10 +0200

    didnt work, guess this will be the more or less final version

--- calc_best_moves.c
+++ calc_best_moves.c
@@ -6,7 +6,7 @@
 /*   By: jdufour <jdufour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 15:45:51 by jdufour           #+#    #+#             */
-/*   Updated: 2023/08/03 18:27:37 by jdufour          ###   ########.fr       */
+/*   Updated: 2023/08/03 18:46:42 by jdufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,33 +40,33 @@ int	good_sup_chunk_check(stack **head_b, stack **i)
 	return (chunk_ref);
 }
 
-int	good_inf_chunk_check(stack **head_b, stack **i)
-{
-	stack   *j;
-	int     chunk_ref;
+// int	good_inf_chunk_check(stack **head_b, stack **i)
+// {
+// 	stack   *j;
+// 	int     chunk_ref;
 	
-	j = *head_b;
-	chunk_ref = (*i)->chunk;
-	while (chunk_ref < ft_stacklast(*head_b)->chunk)
-	{
-		while (j)
-		{
-			if (j->chunk == chunk_ref)
-				return (chunk_ref);
-			j = j->next;
-		}
-		j = *head_b;
-		chunk_ref++;
-	}
-	// if (chunk_ref == ft_stacklast(*head_b)->chunk)
-	// 	while (j)
-	// 	{
-	// 		if (j->chunk < chunk_ref)
-	// 			chunk_ref = j->chunk;
-	// 		j = j->next;
-	// 	}
-	return (-1);
-}
+// 	j = *head_b;
+// 	chunk_ref = (*i)->chunk;
+// 	while (chunk_ref < ft_stacklast(*head_b)->chunk)
+// 	{
+// 		while (j)
+// 		{
+// 			if (j->chunk == chunk_ref)
+// 				return (chunk_ref);
+// 			j = j->next;
+// 		}
+// 		j = *head_b;
+// 		chunk_ref++;
+// 	}
+// 	// if (chunk_ref == ft_stacklast(*head_b)->chunk)
+// 	// 	while (j)
+// 	// 	{
+// 	// 		if (j->chunk < chunk_ref)
+// 	// 			chunk_ref = j->chunk;
+// 	// 		j = j->next;
+// 	// 	}
+// 	return (-1);
+// }
 
 int	calc_best_move_a(stack **i, stack **head_a)
 {
@@ -106,8 +106,8 @@ int calc_best_move_b(stack **i, stack **head_b)
 	{
 		if (j->chunk == good_sup_chunk_check(head_b, i))
 			return (moves);
-		if (j->chunk == good_inf_chunk_check(head_b, i))
-			return (moves + 1);
+		// if (j->chunk == good_inf_chunk_check(head_b, i))
+		// 	return (moves + 1);
 		if (ft_stacksize(*head_b) % 2 == 0)
 		{
 			if (moves == ft_stacksize(*head_b) / 2)
